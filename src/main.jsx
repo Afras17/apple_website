c81dfac18a682e8423f69c6e07ada1167df8fd35
Afras Rahim <afrasrahim11@gamil.com>
2025-03-15 14:52:57 +0530

    error handling fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,28 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://1837716d13c52a9f53d37554785cdeaa@o4508980954923008.ingest.us.sentry.io/4508980959117312",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
