8d76edca17f5da1966a74386f3acc469a041a27f
Afras Rahim <afrasrahim11@gamil.com>
2025-03-15 15:19:39 +0530

    sentry is added

--- src/main.jsx
+++ src/main.jsx
@@ -8,20 +8,6 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://1837716d13c52a9f53d37554785cdeaa@o4508980954923008.ingest.us.sentry.io/4508980959117312",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-    }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0, 
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0, 
 });
 
 createRoot(document.getElementById('root')).render(
